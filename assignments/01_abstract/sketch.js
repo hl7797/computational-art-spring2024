f95cd923e817ae5167064b339a4e6aa9fb5dc03a
hl7797 <li20010829@gmail.com>
2024-01-25 11:43:04 -0500

    1

--- assignments/01_abstract/sketch.js
+++ assignments/01_abstract/sketch.js
@@ -1,7 +1,32 @@
+let dot;
+
+
 function setup() {
- // put setup code here
+  createCanvas(800,600);
+  colorMode(HSB);
+
+  dot = new Dot(100,350,50);
+
 }
 
 function draw() {
-  // put drawing code here
+  //background(0,0,100);
+
+  dot.update();
+
+}
+
+class Dot {
+  constructor(x, y, diameter) {
+    this.x = x;
+    this.y = y;
+    this.diameter = diameter;
+    this.hue = random(360);
+  }
+
+  update() {
+    fill(this.hue, 50, 100);
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+
 }
