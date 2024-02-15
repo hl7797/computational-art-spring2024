a3826a6b27c386efcefc652dd6668f5bf908ffe7
hl7797 <li20010829@gmail.com>
2024-02-15 11:26:39 -0500

    1

---
+++ assignments/03_physics/sketch.js
@@ -0,0 +1,49 @@
+let n = 0; 
+let x;
+  
+function setup() { 
+    createCanvas(600,600); 
+} 
+<<<<<<< Updated upstream
+
+function draw() { 
+    background(0); 
+    let noiseVal = []; 
+    for (let i = 0; i < width; i++) { 
+        noiseVal[i] = noise(n) * height; 
+        n += 0.02; 
+    } 
+  x= color(random(255), random(255), random(255));
+    
+    stroke(x); 
+    noFill(); 
+    beginShape(); 
+    for (let x = 0; x < width; x++) { 
+        vertex(x, noiseVal[x]); 
+    } 
+    endShape(); 
+} 
+
+
+
+
+=======
+
+function draw() { 
+    background(0); 
+    let noiseVal = []; 
+    for (let i = 0; i < width; i++) { 
+        noiseVal[i] = noise(n) * height; 
+        n += 0.02; 
+    } 
+  x= color(random(255), random(255), random(255));
+    
+    stroke(x); 
+    noFill(); 
+    beginShape(); 
+    for (let x = 0; x < width; x++) { 
+        vertex(x, noiseVal[x]); 
+    } 
+    endShape(); 
+} 
+>>>>>>> Stashed changes
