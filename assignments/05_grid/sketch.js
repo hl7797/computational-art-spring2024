58785a303d82c18de250e5f16fb80617e029e93e
hl7797 <li20010829@gmail.com>
2024-02-28 14:18:10 -0500

    1

--- assignments/05_grid/sketch.js
+++ assignments/05_grid/sketch.js
@@ -1,7 +1,24 @@
+let cols, rows;
+let space = 60;
+let noiseScale = 0.05; 
+
 function setup() {
- // put setup code here
+  createCanvas(600, 600);
+  cols = width / space;
+  rows = height / space;
+  colorMode(360, 100, 100); 
 }
 
 function draw() {
-  // put drawing code here
+  background(220);
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      let noiseVal = noise(x * noiseScale, y * noiseScale);
+      let d = map(noiseVal, 0, 1, 50,space); 
+      let colorVal = sin(frameCount * 0.1 + (x + y) *0.2) * 360;
+      fill(colorVal, 100, 100);
+      noStroke();
+      ellipse(x * space + space / 2, y * space + space / 2, d);
+    }
+  }
 }
