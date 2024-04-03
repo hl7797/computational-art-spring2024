2137a9b508021f73b4f22d0db6d00d2de89893b0
hl7797 <li20010829@gmail.com>
2024-04-03 15:50:34 -0400

    1

--- assignments/07_fractals/sketch.js
+++ assignments/07_fractals/sketch.js
@@ -1,7 +1,31 @@
-function setup() {
- // put setup code here
-}
+let angle = 0;
+let direction = 1;
 
+function setup() {
+  createCanvas(600, 600);
+  noFill();
+}
 function draw() {
-  // put drawing code here
+  background(255,20);
+  Text();
+  translate(width/2, height/2);
+  drawSquare(0, 0, 250);
+  angle += 0.008 * direction;
+}
+function drawSquare(x, y, size) {
+  if (size < 5) {
+    return;
+  }
+   strokeWeight(1);
+   stroke(255,100,50);
+   rotate(angle);
+   rect(x, y, size, size);
+   drawSquare(x, y, size/2);
+}
+function mouseClicked() {
+  direction *= -1;
+}
+function Text(){
+    textSize(18);
+    text("Click to change rotation direction", 150, 500);
 }
