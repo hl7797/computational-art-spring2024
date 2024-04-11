9b1142364ecd7bf5fb244c39f4707ad9ce3137f1
hl7797 <li20010829@gmail.com>
2024-04-11 12:00:18 -0400

    1

--- assignments/08_sound/sketch.js
+++ assignments/08_sound/sketch.js
@@ -1,7 +1,17 @@
+let kickSample;
+
+
 function setup() {
  // put setup code here
+ createCanvas(600, 400);
+
 }
 
 function draw() {
   // put drawing code here
+  background(0, 100, 100);
+
+}
+function preload() {
+  kickSample = loadSound("https://hl7797.github.io/computational-art-spring2024/assignments/08_sound/samples/111.wav");
 }
