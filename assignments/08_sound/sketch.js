8335bd33370fdd588831fc0dd43659f83c8ba381
hl7797 <li20010829@gmail.com>
2024-04-16 17:26:02 -0400

    1

--- assignments/08_sound/sketch.js
+++ assignments/08_sound/sketch.js
@@ -1,23 +1,21 @@
-let kickSample;
+let song;
 
-function preload() {
-  kickSample = loadSound("https://hl7797.github.io/computational-art-spring2024/assignments/08_sound/samples/piano.wav");
+
+function setup() 
+  {
+  song = loadSound("https://hl7797.github.io/computational-art-spring2024/assignments/08_sound/samples/piano.wav");
+ // song = loadSound('samples/piano.wav');
+  createCanvas(720, 200);
+  background(255, 0, 0);
 }
-
-function setup() {
-
- createCanvas(600, 400);
-
-}
-
-function draw() {
-  
-  background(0, 100, 100);
-
-}
-
 
 function mousePressed() {
-  //userStartAudio();
-  kickSample.play();
+  if (song.isPlaying()) {
+    // .isPlaying() returns a boolean
+    song.stop();
+    background(255, 0, 0);
+  } else {
+    song.play();
+    background(0, 255, 0);
+  }
 }
